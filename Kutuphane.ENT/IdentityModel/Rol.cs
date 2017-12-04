8131789d97ba357f32da5acd00fda7d20cf7f5bf
Hediye TAŞAR <hediyetasarr@gmail.com>
2017-12-04 17:27:45 +0300

    view

--- Kutuphane.ENT/IdentityModel/Rol.cs
+++ Kutuphane.ENT/IdentityModel/Rol.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kutuphane.ENT.IdentityModel
 {
+    [Table("Roll")]
     public class Rol : IdentityUser
     {
         public string Aciklama { get; set; }
---
+++ Kutuphane.ENT/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.ENT.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="Kullanıcı Adı")]
+        public string KullaniciAdi { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Şifre")]
+        public string Sifre { get; set; }
+    }
+}
