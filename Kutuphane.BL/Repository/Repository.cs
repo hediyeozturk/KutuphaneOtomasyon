a497da36ad0c13bca56a1a4a416088271ceace62
Hediye TAŞAR <hediyetasarr@gmail.com>
2017-12-06 15:01:22 +0300

    Repository

---
+++ Kutuphane.BL/Repository/Repository.cs
@@ -0,0 +1,15 @@
+﻿using Kutuphane.ENT.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.BL.Repository
+{
+    public class UyeRepo : RepositoryBase<Uye, int> { }
+    public class KitapRepo : RepositoryBase<Kitap, int> { }
+    public class YazarRepo : RepositoryBase<Yazar, int> { }
+    public class KategoriRepo : RepositoryBase<Kategori, int> { }
+    public class OduncRepo : RepositoryBase<Odunc, int> { }
+}
