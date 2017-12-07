1f65a7da14037a69b54f317920794eea1e448506
Hediye TAŞAR <hediyetasarr@gmail.com>
2017-12-07 13:13:23 +0300

    .startup

---
+++ Kutuphane.MVC/Controllers/HesapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kutuphane.MVC.Controllers
+{
+    public class HesapController : Controller
+    {
+        // GET: Hesap
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
